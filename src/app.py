88700cb15ff3f4783c8e99a5ee4bc1940943074a
J Zawada <jordzawada@gmail.com>
2020-06-10 13:18:52 -0600

    created to-do outline of coomp 240

---
+++ src/app.py
@@ -0,0 +1,18 @@
+# do this in VS live share
+#harmonize excel sheets
+#4 sheets
+#
+
+# SERVER SIDE CODE (pyhton)
+# pull data from an excel sheet in the repository
+# code that makes an invoice object --> jsonify it
+# this will be @app.route //not class _____ server 
+# only one route to make inovices (invoice/#)
+# based on the #, we will spit out that invoice
+# i don't see a need for a POST at this point
+
+# JS (react, fetch)
+# htttp/// _____/invoice/
+# GET data
+# display as REACT table (flask template)
+
